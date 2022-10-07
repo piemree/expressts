6714f8127419ee76efba91a326fe5dc09f7ee0bc
Emre <pi.emree@gmail.com>
2022-10-08 02:07:27 +0300

    initial command

---
+++ bin/cli.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+const { execSync } = require('child_process');
+const runCommand = (command) => {
+    try {
+        execSync(`${command}`, { stdio: "inherit" })
+    } catch (error) {
+        console.error(`Failed to execute ${command}`, error);
+        return false
+    }
+    return true
+}
+
+const repoName = process.argv[2]
+const gitCheckoutCommand = `git clone --depth 1 origin ${repoName}`
+const installDepsCommand = `cd ${repoName} && npm install`
+
+console.log(`Cloning the repository with name ${repoName}`);
+const chechedOut = runCommand(gitCheckoutCommand)
+if (!chechedOut) process.exit(-1)
+
+console.log(`Installing dependencies for ${repoName}`);
+const installDeps = runCommand(installDepsCommand)
+if (!installDeps) process.exit(-1)
+
+console.log("You are ready. Follow the following commands to start");
+console.log(`cd ${repoName} && npm run dev`);
